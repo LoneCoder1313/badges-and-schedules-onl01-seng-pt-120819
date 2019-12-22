79b7a08209c01e65cff206fc6fd553a80341ceee
Ricky dildy <rdildy92@gmail.com>
2019-12-22 02:04:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,5 @@
-# Write your code here.
+def conference_badges(name=["Arel"], attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Mattz"])
+  conference_badges.each_with_index do |name|
+    puts "Hello my name is #{name}"
+  end
+end 
