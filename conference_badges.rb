f34ad93c5b4cf9d43d534c2ef8e736ad7b361f45
Ricky dildy <rdildy92@gmail.com>
2019-12-22 02:23:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 room_assignment= [1,2,3,4,5,6,7]
 
-def conference_badges(badge_maker(name="Arel"))
+def conference_badges(badge_maker(name="Arel"
 end 
