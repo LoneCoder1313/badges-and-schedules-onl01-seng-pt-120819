2cc5eb98c9d630d6549091e3c5c06624a24aacf8
Ricky dildy <rdildy92@gmail.com>
2019-12-22 02:32:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,8 @@ end
 
 def batch_badge_creator(attendees)
   attendees.each do |attendees|
-  "Hello, my name is #{attendees}."
+    badge_names =[]
+    badge_names << "Hello, my name is #{attendee}."
 end 
+   badge_names
 end 
